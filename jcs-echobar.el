a485112ed539f437e8cf45e8af96d2e48ab2c16e
Jen-Chieh Shen <jcs090218@gmail.com>
2024-01-01 01:12:14 -0800

    chore: Update copyright year

--- jcs-echobar.el
+++ jcs-echobar.el
@@ -1,6 +1,6 @@
 ;;; jcs-echobar.el --- An echo-bar for jcs-emacs  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2023  Shen, Jen-Chieh
+;; Copyright (C) 2023-2024  Shen, Jen-Chieh
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
