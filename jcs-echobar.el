508e96f0a7fec80253a9f402a53f1a11ae873863
JenChieh <jcs090218@gmail.com>
2026-01-28 00:39:37 +0800

    feat: upgrade with click

--- jcs-echobar.el
+++ jcs-echobar.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; URL: https://github.com/jcs-emacs/jcs-echobar
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "29.1") (echo-bar "1.0.0") (indent-control "0.1.0") (show-eol "0.1.0") (keycast "1.2.0"))
+;; Package-Requires: ((emacs "29.1") (elenv "0.1.0") (echo-bar "1.0.0") (indent-control "0.1.0") (show-eol "0.1.0") (keycast "1.2.0"))
 ;; Keywords: faces echo-bar
 
 ;; This file is not part of GNU Emacs.
@@ -31,6 +31,9 @@
 
 ;;; Code:
 
+(require 'time)
+
+(require 'elenv)
 (require 'echo-bar)
 (require 'indent-control)
 (require 'show-eol)
@@ -49,12 +52,17 @@
     (:eval (jcs-echobar--render-eol))
     (:eval (jcs-echobar--render-time)))
   "List of item to render in echo-bar."
-  :type 'list
+  :type '(list symbol)
   :group 'jcs-echobar)
 
-(defcustom jcs-echobar-keycast-format "%K%C%R   "
+(defcustom jcs-echobar-keycast-format "%K%C%R"
   "The keycast format spec."
   :type 'string
+  :group 'jcs-echobar)
+
+(defface jcs-echobar-default
+  '((t nil))
+  "Face for echo-bar."
   :group 'jcs-echobar)
 
 ;;
@@ -69,7 +77,7 @@
   (progn  ; keycast
     (add-hook 'post-command-hook #'keycast--update t)
     (add-hook 'minibuffer-exit-hook #'keycast--minibuffer-exit t)
-    (advice-add 'keycast--update :after #'jcs-echobar--keycast-update))
+    (advice-add 'keycast--update :after #'jcs-echobar-update))
   (add-hook 'window-size-change-functions #'jcs-echobar--window-resize)
   (jcs-echobar--window-resize)  ; call it manually once
   (setq jcs-echobar--default-function echo-bar-function)
@@ -81,7 +89,7 @@
   (progn  ; keycast
     (remove-hook 'post-command-hook #'keycast--update)
     (remove-hook 'minibuffer-exit-hook #'keycast--minibuffer-exit)
-    (advice-remove 'keycast--update #'jcs-echobar--keycast-update))
+    (advice-remove 'keycast--update #'jcs-echobar-update))
   (remove-hook 'window-size-change-functions #'jcs-echobar--window-resize)
   (setq echo-bar-function jcs-echobar--default-function)
   (echo-bar-mode -1))
@@ -109,6 +117,13 @@
 (defun jcs-echobar--buffer-spaces-or-tabs ()
   "Check if buffer using spaces or tabs."
   (if indent-tabs-mode "TAB" "SPC"))
+
+(defmacro jcs-echobar--with-mouse-click (&rest body)
+  "Execute BODY with in the mouse click event."
+  (declare (indent 0))
+  `(let ((map (make-sparse-keymap)))
+     (define-key map [down-mouse-1] (lambda (&rest _) (interactive) ,@body))
+     map))
 
 ;;
 ;; (@* "Core" )
@@ -138,7 +153,7 @@
   "Render the echo-bar."
   (string-trim (mapconcat #'format-mode-line jcs-echobar--render "")))
 
-(defun jcs-echobar--keycast-update (&rest _)
+(defun jcs-echobar-update (&rest _)
   "Exection after `keycast-update' function."
   (when (bound-and-true-p jcs-echobar-mode)
     (echo-bar-update)))
@@ -149,26 +164,53 @@
 
 (defun jcs-echobar--render-spaces-tabs-size ()
   "Render spaces/tabs size."
-  (format "%s: %s  "
-          (jcs-echobar--buffer-spaces-or-tabs)
-          (indent-control-get-indent-level-by-mode)))
+  (concat
+   (propertize (elenv-2str (jcs-echobar--buffer-spaces-or-tabs))
+               'face 'jcs-echobar-default
+               'mouse-face 'mode-line-highlight
+               'local-map (jcs-echobar--with-mouse-click
+                            (indent-tabs-mode (if indent-tabs-mode -1 1))))
+   " "
+   (propertize (elenv-2str (indent-control-get-indent-level-by-mode))
+               'face 'jcs-echobar-default
+               'mouse-face 'mode-line-highlight
+               'local-map (jcs-echobar--with-mouse-click
+                            (call-interactively #'indent-control-set-indent-level-by-mode)))
+   "  "))
 
 (defun jcs-echobar--render-coding-system ()
   "Render buffer coding system."
-  (format "%s  " buffer-file-coding-system))
+  (concat
+   (propertize (elenv-2str buffer-file-coding-system)
+               'face 'jcs-echobar-default
+               'mouse-face 'mode-line-highlight
+               'local-map (jcs-echobar--with-mouse-click
+                            (call-interactively #'set-buffer-file-coding-system)))
+   "  "))
 
 (defun jcs-echobar--render-eol ()
   "Render line-endings."
-  (format "%s  " (show-eol-get-eol-mark-by-system)))
+  (concat
+   (propertize (elenv-2str (show-eol-get-eol-mark-by-system))
+               'face 'jcs-echobar-default
+               'mouse-face 'mode-line-highlight
+               'local-map (jcs-echobar--with-mouse-click
+                            (show-eol-mode (if show-eol-mode -1 1))))
+   "  "))
 
 (defun jcs-echobar--render-time ()
   "Render time."
-  (format-time-string "%b %d, %Y %H:%M:%S"))
+  (propertize (format-mode-line display-time-string)
+              'face 'jcs-echobar-default
+              'mouse-face 'mode-line-highlight
+              'help-echo (format-time-string "%a %b %e, %Y")))
 
 (defun jcs-echobar--render-keycast ()
   "Render `keycast'."
   (when (featurep 'keycast)
-    (keycast--format jcs-echobar-keycast-format)))
+    (concat (propertize (keycast--format jcs-echobar-keycast-format)
+                        'face 'jcs-echobar-default)
+            "   ")))
 
 (provide 'jcs-echobar)
 ;;; jcs-echobar.el ends here
